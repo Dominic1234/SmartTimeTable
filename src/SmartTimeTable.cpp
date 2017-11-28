7904f96c62da6ea8976d9e45f276b66df1fe3c9b
Dominic1234 <dhruv.sharma.017@gmail.com>
2017-11-28 18:14:46 +0530

    First Commit

---
+++ src/SmartTimeTable.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : SmartTimeTable.cpp
+// Author      : dhruv
+// Version     :
+// Copyright   : copyright
+// Description : A smart time table
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "School Project" << endl; // prints School Project
+	return 0;
+}
